87b53eed47fc640cd5ec5ee26defcd9710b39c6a
abhishek1khanna <140689240+abhishek1khanna@users.noreply.github.com>
2025-02-22 16:22:36 +0530

    changes to hello word again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js in Docker!!!!! ----1111 33333333');
+  res.send('Hello from Node.js in Docker!!!!! ----1111 33333333 7777');
 });
 
 app.listen(PORT, () => {
